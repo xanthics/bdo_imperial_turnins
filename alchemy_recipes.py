9b43ccd7d2d36c9f66768fd5f13d0840d08184ac
xanthics <3137623+xanthics@users.noreply.github.com>
2019-11-09 16:26:33 -0800

    Added support for alchemy

---
+++ alchemy_recipes.py
@@ -0,0 +1,115 @@
+alchemy = {
+	"Apprentice's Medicine Box": {
+		"base_value": 52000,
+		"reward": "[Alchemy] Small Delivery Gift Box",
+		"materials": {
+			"Resurrection Elixir": 21,
+			"Strong Resurrection Elixir": 7,
+			"Elixir of Life": 12,
+			"Elixir of Strong Life": 4,
+			"Elixir of Seal": 12,
+			"Agile Seal Elixir": 4,
+			"Elixir of Energy": 12,
+			"Surging Energy Elixir": 4,
+			"Elixir of Mentality": 15,
+			"Elixir of Clear Mentality": 5,
+			"Elixir of Will": 12,
+			"Elixir of Remarkable Will": 4
+		}
+	},
+	"Skilled Alchemist's Medicine Box": {
+		"base_value": 80000,
+		"reward": "[Alchemy] Plain Delivery Gift Box",
+		"materials": {
+			"Exp Elixir": 21,
+			"Splendid Exp Elixir": 7,
+			"Elixir of Shock": 18,
+			"Elixir of Strong Shock": 6,
+			"Elixir of Demihuman Hunt": 18,
+			"Fierce Demihuman Hunting Elixir": 6,
+			"Worker's Elixir": 24,
+			"Skilled Worker's Elixir": 8,
+			"Elixir of Human Hunt": 18,
+			"Elixir of Perfect Human Hunt": 6,
+			"Elixir of Resistance": 12,
+			"Elixir of Sharp Resistance": 4
+		}
+	},
+	"Professional's Medicine Box": {
+		"base_value": 120000,
+		"reward": "[Alchemy] Resplendent Delivery Gift Box",
+		"materials": {
+			"Elixir of Fury": 15,
+			"Elixir of Endless Fury": 5,
+			"Fisher's Elixir": 15,
+			"Skilled Fisher's Elixir": 5,
+			"Defense Elixir": 15,
+			"Elixir of Steel Defense": 5,
+			"Elixir of Wind": 15,
+			"Elixir of Flowing Wind": 5,
+			"Elixir of Training": 15,
+			"Elixir of Expert Training": 5,
+			"Elixir of Time": 15,
+			"Elixir of Flowing Time": 5,
+			"Weenie Elixir": 15,
+			"Surging Weenie Elixir": 5
+		}
+	},
+	"Artisan's Medicine Box": {
+		"base_value": 160000,
+		"reward": "[Alchemy] Splendid Delivery Gift Box",
+		"materials": {
+			"Elixir of Swiftness": 15,
+			"Elixir of Intrepid Swiftness": 5,
+			"Elixir of Concentration": 27,
+			"Elixir of Advanced Concentration": 9,
+			"Looney Elixir": 12,
+			"Reliable Looney Elixir": 4,
+			"Elixir of Spells": 15,
+			"Elixir of Agile Spells": 5,
+			"Elixir of Amity": 15,
+			"Improved Elixir of Amity": 5,
+			"Elixir of Endurance": 24,
+			"Elixir of Overwhelming Endurance": 8,
+			"Elixir of Draining": 18,
+			"Elixir of Strong Draining": 6
+		}
+	},
+	"Master's Medicine Box": {
+		"base_value": 220000,
+		"reward": "[Alchemy] Special Delivery Gift Box",
+		"materials": {
+			"Elixir of Wings": 27,
+			"Soaring Wings Elixir": 9,
+			"Elixir of Assassination": 15,
+			"Elixir of Lethal Assassination": 5,
+			"Golden Hand Elixir": 12,
+			"Glorious Golden Hand Elixir": 4,
+			"Helix Elixir": 12,
+			"Splendid Helix Elixir": 4,
+			"Marking Reagent": 12,
+			"Elixir of Perforation": 9,
+			"Elixir of Brutal Perforation": 3,
+			"Elixir of Carnage": 9,
+			"Elixir of Brutal Carnage": 3
+		}
+	},
+	"Guru's Medicine Box": {
+		"base_value": 320000,
+		"reward": "[Alchemy] Shiny Delivery Gift Box",
+		"materials": {
+			"Elixir of Death": 9,
+			"Brutal Death Elixir": 3,
+			"Grim Reaper's Elixir": 9,
+			"Grim Soul Reaper's Elixir": 3,
+			"Griffon's Elixir": 9,
+			"Strong Griffon's Elixir": 3,
+			"Elixir of Frenzy": 9,
+			"Elixir of Endless Frenzy": 3,
+			"Elixir of Sky": 9,
+			"Merciless Sky Elixir": 3,
+			"Elixir of Detection": 9,
+			"Elixir of Sharp Detection": 3
+		}
+	},
+}
